dd34ba19b3702ad5b01583ae8be9e10ec08266ba
galihekayuwana.office <galihekayuwana.office@gmail.com>
2022-09-21 10:39:03 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,144 @@
+import React, {useState, useEffect} from 'react';
+import {
+  View,
+  Text,
+  StatusBar,
+  Image,
+  TouchableOpacity,
+  ImageBackground,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome5';
+import DefaultImage from './image/profile.jpeg';
+
+const App = () => {
+  const DEFAULT_IMAGE = Image.resolveAssetSource(DefaultImage).uri;
+  return (
+    <View style={{flex: 1}}>
+      <StatusBar barStyle={'light-content'} backgroundColor="#212121" />
+      <ImageBackground
+        source={{
+          uri: 'https://images.unsplash.com/photo-1507281736509-c6289f1ea0f8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+        }}
+        style={{flex: 0.5}}
+        resizeMode={'cover'}>
+        <View style={{flex: 0.5}}></View>
+      </ImageBackground>
+      <View style={{flex: 1, backgroundColor: '#FFFFFF'}}>
+        <View style={{justifyContent: 'center', alignItems: 'center'}}>
+          <Image
+          source={{uri: DEFAULT_IMAGE}}
+            style={{
+              width: 100,
+              height: 100,
+              borderRadius: 100 / 2,
+              borderWidth: 3,
+              borderColor: '#FFFFFF',
+              position: 'absolute',
+              zIndex: 2,
+            }}
+          />
+        </View>
+        <View style={{marginTop: 60}}>
+          <Text
+            style={{
+              fontWeight: 'bold',
+              fontSize: 18,
+              textAlign: 'center',
+              color: '#212121',
+            }}>Galih Eka Yuwana
+          </Text>
+          <Text style={{textAlign: 'center'}}>
+            XII RPL-A   |   SMKN 2 SURAKARTA
+          </Text>
+          <View style={{marginLeft: 80}}>
+            <View
+              style={{
+                flexDirection: 'row',
+                justifyContent: 'center',
+                alignItems: 'center',
+                marginTop: 20,
+              }}>
+              <View
+                style={{
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  width: 40,
+                  height: 40,
+                }}>
+                <Icon name="phone" size={25} color="#212121" />
+              </View>
+              <View style={{justifyContent: 'center', marginLeft: 10, flex: 1}}>
+                <Text style={{fontWeight: 'bold'}}>081233559837</Text>
+              </View>
+            </View>
+            <View
+              style={{
+                flexDirection: 'row',
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}>
+              <View
+                style={{
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  width: 40,
+                  height: 40,
+                }}>
+                <Icon name="map-marker-alt" size={25} color="#212121" />
+              </View>
+              <View style={{justifyContent: 'center', marginLeft: 10, flex: 1}}>
+                <Text style={{fontWeight: 'bold'}}>Sukohaarjo, Jawa Tengah</Text>
+              </View>
+            </View>
+            <View
+              style={{
+                flexDirection: 'row',
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}>
+              <View
+                style={{
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  width: 40,
+                  height: 40,
+                }}>
+                <Icon name="envelope" size={25} color="#212121" />
+              </View>
+              <View style={{justifyContent: 'center', marginLeft: 10, flex: 1}}>
+                <Text style={{fontWeight: 'bold'}}>
+                  galihyuwana44@gmail.com
+                </Text>
+              </View>
+            </View>
+          </View>
+          <View
+            style={{flexDirection: 'row', marginTop: 40, marginHorizontal: 30}}>
+            <TouchableOpacity
+              style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+              <Icon name="facebook" size={25} color="#bdbdbd" />
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+              <Icon name="instagram" size={25} color="#bdbdbd" />
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+              <Icon name="github" size={25} color="#bdbdbd" />
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+              <Icon name="twitter" size={25} color="#bdbdbd" />
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+              <Icon name="linkedin" size={25} color="#bdbdbd" />
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default App;
